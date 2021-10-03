2d586679294094a9e430d9d9ade5764946678a7e
Harshank Patel <astroid169@gmail.com>
2021-10-03 17:26:30 -0500

    Added first integration test
    Added first integration test

--- spec/feature/integration_spec.rb
+++ spec/feature/integration_spec.rb
@@ -1,3 +1,26 @@
 # location: spec/feature/integration_spec.rb
 require 'rails_helper'
 
+RSpec.describe 'Creating an appoinment!', type: :feature do
+    scenario 'valid xyz....' do
+
+    visit new_user_path
+    print "---------new user page------------\n"
+    print page.body
+    print "---------new user page------------\n"
+
+    fill_in 'user_name', with: 'harry potter'
+    fill_in 'user_phoneNumber', with: '007'
+    fill_in 'user_email', with: 'harry_potter@tamu.edu'
+    fill_in 'user_interviewDate', with: '01/02/2021'
+    click_on 'Create User'
+    
+    visit @user
+
+    print "---------@user page start------------\n"
+    print page.body
+    print "---------@user page end------------\n"
+    
+    expect(page).to have_content('harry potter')
+    end
+end
