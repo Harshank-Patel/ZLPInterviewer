15d95227c7ba70834e6c03197e051e7c54405e4d
Yodafire18 <anu.srini21@gmail.com>
2021-10-23 14:38:24 -0500

    Unit tests updated

--- spec/unit/unit_spec.rb
+++ spec/unit/unit_spec.rb
@@ -3,34 +3,34 @@ require 'rails_helper'
 
 RSpec.describe User, type: :model do
   subject do
-    described_class.new(name: 'David Erdner', phoneNumber: '111-111-1111', email: 'derdner@tamu.edu', interviewDate: '10/7/2021', interviewTime: '8:00am')
+    described_class.new(name: 'David Erdner', phoneNumber: '111-111-1111', email: 'derdner@tamu.edu', interviewDateTime: '10/7/2021 , 8:00am')
   end
 
   
   it 'is valid name with valid attributes' do
-    name = User.new(name: 'David Erdner', phoneNumber: '111-111-1111', email: 'derdner@tamu.edu', interviewDate: '10/7/2021', interviewTime: '8:00am')
+    name = User.new(name: 'David Erdner', phoneNumber: '111-111-1111', email: 'derdner@tamu.edu', interviewDateTime: '10/7/2021 , 8:00am')
     expect(name).to be_valid
   end
 
   
   it 'is valid phonenumber with valid attributes' do
-    phoneNumber = User.new(phoneNumber:'111-111-1111', name: 'David Erdner', email: 'derdner@tamu.edu', interviewDate: '10/7/2021', interviewTime: '8:00am')
+    phoneNumber = User.new(phoneNumber:'111-111-1111', name: 'David Erdner', email: 'derdner@tamu.edu', interviewDateTime: '10/7/2021 , 8:00am')
     expect(phoneNumber).to be_valid
   end
 
 
   it 'is valid email with valid attributes' do
-    email = User.new(email: 'derdner@tamu.edu', name: 'David Erdner', phoneNumber: '111-111-1111', interviewDate: '10/7/2021', interviewTime: '8:00am')
+    email = User.new(email: 'derdner@tamu.edu', name: 'David Erdner', phoneNumber: '111-111-1111', interviewDateTime: '10/7/2021 , 8:00am')
     expect(email).to be_valid
   end
 
   it 'is valid interviewDate with valid attributes' do
-    interviewDate = User.new(interviewDate: 10/7/2021, name: 'David Erdner', phoneNumber: '111-111-1111', email: 'derdner@tamu.edu', interviewTime: '8:00am') 
+    interviewDate = User.new(interviewDateTime: '10/7/2021 , 8:00am', name: 'David Erdner', phoneNumber: '111-111-1111', email: 'derdner@tamu.edu') 
     expect(interviewDate).to be_valid
   end
 
   it 'is valid interviewTime with valid attributes' do
-    interviewTime = User.new(interviewTime: '8:00am', name: 'David Erdner', phoneNumber: '111-111-1111', email: 'derdner@tamu.edu', interviewDate: '10/7/2021')
+    interviewTime = User.new(interviewDateTime: '10/7/2021 , 8:00am', name: 'David Erdner', phoneNumber: '111-111-1111', email: 'derdner@tamu.edu')
     expect(interviewTime).to be_valid
   end
 end
