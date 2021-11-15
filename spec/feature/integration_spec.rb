998287f8ec928ecccbbcd9ff155fc4bebe97e00c
Yodafire18 <anu.srini21@gmail.com>
2021-11-15 16:16:57 -0600

    Started basic test suite

--- spec/feature/integration_spec.rb
+++ spec/feature/integration_spec.rb
@@ -1,36 +1,150 @@
+# frozen_string_literal: true
+
 # location: spec/feature/integration_spec.rb
 require 'rails_helper'
 
 RSpec.describe 'Creating an appoinment!', type: :feature do
-    scenario 'Valid Interviewee Page' do
-        visit new_user_path
-        fill_in 'user_name', with: 'harry potter'
-        fill_in 'user_phoneNumber', with: '007'
-        fill_in 'user_email', with: 'harry_potter@tamu.edu'
-        fill_in 'user_interviewDate', with: '01/02/2021'
-        click_on 'Create User'
-        visit @user
-        expect(page).to have_content('harry potter')
-        expect(page).to have_content('007')
-        expect(page).to have_content('harry_potter@tamu.edu')
-        expect(page).to have_content('2021-02-01')
-    end
-    
+  scenario 'Valid Interviewee Page' do
+    visit new_admin_path
+    fill_in 'admin_scheduleName', with: 'Fall 2021'
+    fill_in 'admin_dateRange', with: '2021-10-05:2021-10-08'
+    fill_in 'admin_timeRange', with: '8:00am-3:00pm'
+    fill_in 'admin_interviewLength', with: '30min'
+    fill_in 'admin_numBreaks', with: '3'
+    fill_in 'admin_numRooms', with: '2'
+    click_on 'Create Admin'
+
+    visit new_user_path
+    fill_in 'user_name', with: 'David Tang'
+    fill_in 'user_phoneNumber', with: '281-301-9696'
+    fill_in 'user_email', with: 'davidtang@tamu.edu'
+    choose('user_interviewDateTime_2021-10-07900am')
+
+    click_on 'Submit'
+    visit @user
+    expect(page).to have_content('David Tang')
+    expect(page).to have_content('281-301-9696')
+    expect(page).to have_content('davidtang@tamu.edu')
+    expect(page).to have_content('2021-10-07')
+    expect(page).to have_content('9:00am')
+  end
+
+  scenario 'Valid Admin Page' do
+    visit new_admin_path
+    fill_in 'admin_scheduleName', with: 'Fall 2021'
+    fill_in 'admin_dateRange', with: '2021-10-05:2021-10-08'
+    fill_in 'admin_timeRange', with: '8:00am-5:00pm'
+    fill_in 'admin_interviewLength', with: '30min'
+    fill_in 'admin_numBreaks', with: '3'
+    fill_in 'admin_numRooms', with: '2'
+    click_on 'Create Admin'
+    expect(page).to have_content('Fall 2021')
+    expect(page).to have_content('2021-10-05:2021-10-08')
+    expect(page).to have_content('8:00am-5:00pm')
+    expect(page).to have_content('30min')
+    expect(page).to have_content('3') 
+    expect(page).to have_content('2')
+  end
+
+  scenario 'Delete Admin Page' do
+    visit new_admin_path
+    fill_in 'admin_scheduleName', with: 'Fall 2021'
+    fill_in 'admin_dateRange', with: '2021-10-05:2021-10-08'
+    fill_in 'admin_timeRange', with: '8:00am-5:00pm'
+    fill_in 'admin_interviewLength', with: '30min'
+    fill_in 'admin_numBreaks', with: '3'
+    fill_in 'admin_numRooms', with: '2'
+    click_on 'Create Admin'
+    click_on 'Back'
+    click_on 'Destroy'
+    expect(page).to have_content('Admin was successfully destroyed.')
+  end
 
 
+scenario 'Update Admin Page' do
+  visit new_admin_path
+  fill_in 'admin_scheduleName', with: 'Fall 2021'
+  fill_in 'admin_dateRange', with: '2021-10-05:2021-10-08'
+  fill_in 'admin_timeRange', with: '8:00am-5:00pm'
+  fill_in 'admin_interviewLength', with: '30min'
+  fill_in 'admin_numBreaks', with: '3'
+  fill_in 'admin_numRooms', with: '2'
+  click_on 'Create Admin'
+  click_on 'Back'
+  click_on 'Edit Schedule'
+  click_on 'Update Admin'
+  expect(page).to have_content('Admin was successfully updated.')
+  end
 
-    scenario 'Valid Admin Page' do
+scenario 'Edit Interviewee' do
+    visit new_admin_path
+      fill_in 'admin_scheduleName', with: 'Fall 2021'
+      fill_in 'admin_dateRange', with: '2021-10-05:2021-10-08'
+      fill_in 'admin_timeRange', with: '8:00am-5:00pm'
+      fill_in 'admin_interviewLength', with: '30min'
+      fill_in 'admin_numBreaks', with: '3'
+      fill_in 'admin_numRooms', with: '2'
+      click_on 'Create Admin'
+      click_on 'Back'
+      click_on 'Edit Schedule'
+      click_on 'Update Admin'
+      expect(page).to have_content('Admin was successfully updated.')
+  
+      visit new_user_path
+      fill_in 'user_name', with: 'David Tang'
+      fill_in 'user_phoneNumber', with: '281-301-9696'
+      fill_in 'user_email', with: 'davidtang@tamu.edu'
+      choose('user_interviewDateTime_2021-10-07900am')
+      click_on 'Submit'
+  
+      visit admins_path
+      click_on 'Edit Interviewee'
+      fill_in 'user_name', with: 'David Tang'
+      fill_in 'user_phoneNumber', with: '281-301-9696'
+      fill_in 'user_email', with: 'davidtang@tamu.edu'
+      choose('user_interviewDateTime_2021-10-07900am')
+      click_on 'Submit'
+      end
+      scenario 'Update Admin Page' do
         visit new_admin_path
-        fill_in 'admin_scheduleName', with: 'Fall 2021' 
-        fill_in 'admin_dateRange', with: '09/08/2021-09/29/2021' 
-        fill_in 'admin_timeRange', with: '8:00am-3:00pm' 
-        fill_in 'admin_interviewLength', with: '30min' 
-        fill_in 'admin_numBreaks', with: '3' 
+        fill_in 'admin_scheduleName', with: 'Fall 2021'
+        fill_in 'admin_dateRange', with: '2021-10-05:2021-10-08'
+        fill_in 'admin_timeRange', with: '8:00am-5:00pm'
+        fill_in 'admin_interviewLength', with: '30min'
+        fill_in 'admin_numBreaks', with: '3'
+        fill_in 'admin_numRooms', with: '2'
         click_on 'Create Admin'
-        expect(page).to have_content('Fall 2021')
-        expect(page).to have_content('09/08/2021-09/29/2021')
-        expect(page).to have_content('8:00am-3:00pm')
-        expect(page).to have_content('30min')
-        expect(page).to have_content('3')
-    end
+        click_on 'Back'
+        click_on 'Edit Schedule'
+        click_on 'Update Admin'
+        expect(page).to have_content('Admin was successfully updated.')
+        end
+      
+      scenario 'Destroy Interviewee' do
+          visit new_admin_path
+            fill_in 'admin_scheduleName', with: 'Fall 2021'
+            fill_in 'admin_dateRange', with: '2021-10-05:2021-10-08'
+            fill_in 'admin_timeRange', with: '8:00am-5:00pm'
+            fill_in 'admin_interviewLength', with: '30min'
+            fill_in 'admin_numBreaks', with: '3'
+            fill_in 'admin_numRooms', with: '2'
+            click_on 'Create Admin'
+            click_on 'Back'
+            click_on 'Edit Schedule'
+            click_on 'Update Admin'
+            expect(page).to have_content('Admin was successfully updated.')
+        
+            visit new_user_path
+            fill_in 'user_name', with: 'David Tang'
+            fill_in 'user_phoneNumber', with: '281-301-9696'
+            fill_in 'user_email', with: 'davidtang@tamu.edu'
+            choose('user_interviewDateTime_2021-10-07900am')
+            click_on 'Submit'
+            visit users_path
+            visit admins_path
+            puts page.body
+            click_on 'Destroy Interviewee'
+            
+            end
+      
 end
